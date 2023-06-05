289aee0f626401764fe105d6435a2aa5a11f1434
sam caldwell <mail@samcaldwell.net>
2023-06-05 16:24:50 -0500

    initial commit moving orderedSet from personal repo

---
+++ orderedSet.Add.go
@@ -0,0 +1,17 @@
+package orderedset
+
+import (
+	"fmt"
+	"reflect"
+)
+
+// Add - add item to set.
+func (set *Set) Add(item interface{}) error {
+	if len(set.data) == 0 {
+		set.typ = reflect.TypeOf(item)
+	} else if reflect.TypeOf(set.data[0]) != reflect.TypeOf(item) {
+		return fmt.Errorf("item must be of type %v", reflect.TypeOf(item).String())
+	}
+	set.data = append(set.data, item)
+	return nil
+}
---
+++ orderedSet.Count.go
@@ -0,0 +1,6 @@
+package orderedset
+
+// Count - return a count of the records in a set
+func (set *Set) Count() int {
+	return len(set.data)
+}
